4f3dcaddb01003f29ee0f96efbee69ab88c79157
WebZerefose <anything.codable@gmail.com>
2023-06-23 16:05:35 -0300

    cleanup

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+	return (
+		<View className='flex-1 items-center justify-center bg-white'>
+			<Text>Open up App.js to start working on your app!</Text>
+		</View>
+	)
+}
+
+export default App
+
+const styles = StyleSheet.create({})
